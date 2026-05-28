1fd05a09e71587f630850118422ec6b578618982
lucielialmeida-ops <lucieli.almeida@escola.pr.gov.br>
2026-05-28 14:21:59 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Botão de "Saiba Mais" muda cor quando clicado
+const btn = document.getElementById('btnSaibaMais');
+
+btn.addEventListener('click', () => {
+    alert('Descubra como fortalecer a agricultura de forma sustentável!');
+    btn.style.backgroundColor = '#81c784';
+});
